2bc7d4fa123e702b56768edfb3d68588f9ce5949
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-07-06 21:35:30 +0300

    added content and styled homepage

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,6 +1,28 @@
 import React from "react";
+import "./Homepage.css";
+import { Link } from "react-router-dom";
 const Homepage = () => {
-  return <div></div>;
+  return (
+    <div className="homepage">
+      <div className="homepage-text">
+        <p className="headline">
+          Kotikokki connects the community with home-made food.
+        </p>
+        <p className="homepage-content">
+          If cooking is your hobby, passion or you simply want to share your
+          food with others as well as reduce food waste, Kotikokki is for you.
+        </p>
+        <button className="start-button">
+          <Link to="/signup">Get Started</Link>
+        </button>
+      </div>
+      <img
+        className="homepage-img"
+        src="https://source.unsplash.com/6MT4_Ut8a3Y"
+        alt="home-page"
+      />
+    </div>
+  );
 };
 
 export default Homepage;
