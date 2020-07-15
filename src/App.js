9dfcef3b69e63c0b8f1e760de1c95ff094d2701c
Ilari Laitinen <iim.laitinen@gmail.com>
2020-07-16 00:29:05 +0300

    made homepage = about page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
-import About from "./Pages/About";
 import Blog from "./Pages/Blog";
 import Contact from "./Pages/Contact";
 import Login from "./Pages/Login";
@@ -18,7 +17,7 @@ const App = () => {
         <main>
           <Switch>
             <Route exact path="/" component={Homepage} />
-            <Route path="/about" component={About} />
+            <Route path="/about" component={Homepage} />
             <Route path="/howitworks" component={Howitworks} />
             <Route path="/blog" component={Blog} />
             <Route path="/contact" component={Contact} />
--- src/Pages/About.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const About = () => {
-  return <div>TEST</div>;
-};
-
-export default About;
